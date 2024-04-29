6f29ab02ed630946014a9064c87fc08e5936af62
Faller29 <148046807+Faller29@users.noreply.github.com>
2024-04-29 10:33:54 +0800

    Update camerapage.dart
    buttons, alignment, Scrolls and such

--- healthlens/lib/camerapage.dart
+++ healthlens/lib/camerapage.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:iconly/iconly.dart';
 import 'package:image_picker/image_picker.dart';
 
@@ -16,28 +17,40 @@ class _CameraPageState extends State<CameraPage> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
+      child: ListView(
         children: [
-          Icon(
-            IconlyLight.chart,
-            size: 120,
-            color: Colors.black,
-          ),
-          Text(
-            'Analytics Page',
-            style: TextStyle(
-                color: Colors.black, fontSize: 30, fontWeight: FontWeight.w700),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                IconlyLight.chart,
+                size: 120,
+                color: Colors.black,
+              ),
+              Text(
+                'Camera Page',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 30,
+                    fontWeight: FontWeight.w700),
+              ),
+            ],
           ),
           MaterialButton(
+            elevation: 4,
+            color: Colors.blueAccent,
             child: Text('Camera'),
             onPressed: () {
               _pickImageFromCamera();
             },
           ),
-          SizedBox(
-            height: 20,
+          MaterialButton(
+            elevation: 4,
+            color: Colors.blueAccent,
+            child: Text('Gallery'),
+            onPressed: () {
+              _pickImageFromGallery();
+            },
           ),
           _selectedImage != null ? Image.file(_selectedImage!) : Text('data'),
         ],
@@ -48,6 +61,14 @@ class _CameraPageState extends State<CameraPage> {
   Future _pickImageFromCamera() async {
     final returnedImage =
         await ImagePicker().pickImage(source: ImageSource.camera);
+    setState(() {
+      _selectedImage = File(returnedImage!.path);
+    });
+  }
+
+  Future _pickImageFromGallery() async {
+    final returnedImage =
+        await ImagePicker().pickImage(source: ImageSource.gallery);
     setState(() {
       _selectedImage = File(returnedImage!.path);
     });
