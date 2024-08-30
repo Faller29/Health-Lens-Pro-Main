b0e047308e483faab86ae7a80a16d71e5922b976
Faller29 <148046807+Faller29@users.noreply.github.com>
2024-08-30 15:26:53 +0800

    firebase installation

--- healthlens/lib/main.dart
+++ healthlens/lib/main.dart
@@ -1,10 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:healthlens/calendar_history.dart';
 import 'package:healthlens/entry_point.dart';
+import 'package:healthlens/firebase_options.dart';
 import 'login_page.dart';
 import 'setup.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
